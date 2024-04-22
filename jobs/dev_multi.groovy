c7e3782cef27a3f7c8e560e980b18cebae838fe4
Szu-Ching Peckner <scpeckner@gmail.com>
2024-04-22 12:33:07 -0700

    test

--- jobs/dev_multi.groovy
+++ jobs/dev_multi.groovy
@@ -1,15 +1,15 @@
 org = 'scpeckner'
-repoToTrack = 'jenkins'
-
-multibranchPipelineJob('multi1') {
-    branchSources {
-        git {
-          remote("https://github.com/${org}/${repoToTrack}.git")
-            credentialsId('scpeckner-up') // If using credentials
-            includes('*') // Match all branches
-            id= org+'_'+repoToTrack
-
+reposToTrack = ['jenkins']
+branchToTrack = 'main develop feature* hostfix*'
+reposToTrack.each { repo ->
+    multibranchPipelineJob(repo+'_multi') {
+        branchSources {
+            git {
+                remote("https://github.com/${org}/${repo}.git")
+                credentialsId('scpeckner-up') // If using credentials
+                includes(branchToTrack) // Match all branches
+                id= org+'_'+repo
+            }
         }
     }
-
 }
