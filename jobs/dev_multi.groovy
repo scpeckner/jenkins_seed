52d6f4f5f35a5f7cdda205255ccf0111824f45d8
Szu-Ching Peckner <scpeckner@gmail.com>
2024-04-22 11:17:46 -0700

    test

---
+++ jobs/dev_multi.groovy
@@ -0,0 +1,21 @@
+job('seed1'){
+    description('seed job')
+    concurrentBuild(false)
+    scm {
+        git {
+            remote {
+                url('https://github.com/scpeckner/jenkins_seed.git')
+                credentials('scpeckner-sp')
+            }
+            branch('main')
+        }
+    }
+    steps {
+        dsl {
+            external('jobs/*.groovy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+}
